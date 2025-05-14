b03a38a223309ead23361fe66c95fb193b795133
nattanjunior <fnatanieljunior@gmail.com>
2025-05-14 19:05:55 -0300

    feat: created dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import type { Roles } from "@prisma/client";
+
+// data transfer object
+export class CreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+  role: Roles;
+}
