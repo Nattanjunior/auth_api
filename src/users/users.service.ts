3dae8e3495c0cb7648c114c5a4df22b8b3ada3cd
nattanjunior <fnatanieljunior@gmail.com>
2025-05-14 19:09:20 -0300

    feat: funcionalidades de CRUD de usuarios feita com sucesso

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,30 +2,46 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
-
   constructor(private prisma: PrismaService) { }
 
-
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: bcrypt.hashSync(createUserDto.password, 10)
+      }
+    })
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id }
+    })
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        ...updateUserDto,
+        password: updateUserDto.password
+          ? bcrypt.hashSync(updateUserDto.password, 10)
+          : undefined,
+      }
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.prisma.user.delete({
+      where: { id }
+    });
   }
 }
