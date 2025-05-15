94d83370901311031d1da50f012a1c7b82d661d4
nattanjunior <fnatanieljunior@gmail.com>
2025-05-15 02:10:25 -0300

    feat: add role decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,8 +3,12 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { RequiredRoles } from 'src/auth/required-roles.decorator';
+import { Roles } from '@prisma/client';
+import { RoleGuard } from 'src/auth/role/role.guard';
 
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
+@RequiredRoles(Roles.ADMIN)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
