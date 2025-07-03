e5c05c3991a7c01d2e19bb56a91d722b7fccc3e4
nattanjunior <fnatanieljunior@gmail.com>
2025-07-03 19:21:20 -0300

    fix(posts): define entidade Post com propriedades e decorators para Swagger

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,24 @@
-export class Post {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Post {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  authorId: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
